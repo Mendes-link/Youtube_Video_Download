22b2f484d4ffcf5791c1dca2213871338342f587
Vitor Mendes <vitorsouzamendes24@gmail.com>
2022-08-01 12:38:03 -0300

    first commit

---
+++ youtube_download.py
@@ -0,0 +1,16 @@
+# Instalando a biblioteca "Pytube".
+from pytube import YouTube, streams
+
+# Para inserir o link do vídeo do Youtube.
+print("[Download Vídeo do Youtube!]")
+link = input("Insira o link do vídeo: ")
+video = YouTube(link)
+
+# Para mostrar o título do vídeo e que ja iniciou o download.
+print("Titulo = ", video.title)
+print("Baixando...")
+
+# Para baixar a maior resolução do vídeo.
+stream = video.streams.get_highest_resolution()
+stream.download(output_path="D:\Vitor\Videos\Downloads_Youtube")
+print("Download concluído com sucesso!")
